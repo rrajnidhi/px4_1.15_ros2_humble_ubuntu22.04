cb8c32a79a82404fdcbefd2ff26916b2f8aae9ab
nidhi-aifi <nidhi.r@aifitechnologies.com>
2025-11-19 15:23:29 +0400

    some corrections

--- launch_files/camera_detection.sh
+++ launch_files/camera_detection.sh
@@ -10,6 +10,7 @@ PX4_SYS_AUTOSTART=${PX4_SYS_AUTOSTART:-4002}
 PX4_UXRCE_DDS_PORT=${PX4_UXRCE_DDS_PORT:-8888}
 PX4_UXRCE_DDS_NS=${PX4_UXRCE_DDS_NS:-cam_drone}
 PX4_UAV_MODEL=${PX4_UAV_MODEL:-x500_depth}
+PX4_SYS_AUTOSTART=${PX4_SYS_AUTOSTART:-4002}
 
 # Kill any existing session
 tmux kill-session -t $SESSION_NAME 2>/dev/null
@@ -18,7 +19,7 @@ tmux kill-session -t $SESSION_NAME 2>/dev/null
 tmux new-session -d -s $SESSION_NAME
 
 # Pane 0: PX4 SITL + Gazebo
-tmux send-keys -t $SESSION_NAME "cd /app/PX4-Autopilot/; ROS_DOMAIN_ID=$ROS_DOMAIN_ID PX4_UXRCE_DDS_PORT=$PX4_UXRCE_DDS_PORT PX4_UXRCE_DDS_NS=$PX4_UXRCE_DDS_NS PX4_GZ_MODEL_POSE="268.08,-128.22,3.86,0.00,0,-0.7" PX4_GZ_MODEL=$PX4_UAV_MODEL ./build/px4_sitl_default/bin/px4" C-m
+tmux send-keys -t $SESSION_NAME "cd /app/PX4-Autopilot/; HEADLESS=0 ROS_DOMAIN_ID=$ROS_DOMAIN_ID PX4_UXRCE_DDS_PORT=$PX4_UXRCE_DDS_PORT PX4_UXRCE_DDS_NS=$PX4_UXRCE_DDS_NS PX4_SYS_AUTOSTART=PX4_SYS_AUTOSTART PX4_GZ_MODEL_POSE="268.08,-128.22,3.86,0.00,0,-0.7" PX4_GZ_MODEL=$PX4_UAV_MODEL ./build/px4_sitl_default/bin/px4" C-m
 
 # Pane 1: Micro XRCE-DDS Agent with wait-for-SITL to start logic
 tmux split-window -h -t $SESSION_NAME
@@ -30,13 +31,13 @@ done
 echo 'PX4 SITL detected. Starting Micro XRCE-DDS Agent...'
 MicroXRCEAgent udp4 -p $PX4_UXRCE_DDS_PORT" C-m
 
-# Pane 2: Object detector
+# Pane 2: image bridge ros2 to px4
 tmux split-window -v -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.2 "ros2 run ros_gz_image image_bridge /camera" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "sleep 6 && ros2 run ros_gz_image image_bridge /camera" C-m
 
 # Pane 3: for future use
 tmux split-window -v -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.2 "echo 'This pane is reserved for later use'" C-m
+tmux send-keys -t $SESSION_NAME:0.3 "echo 'This pane is reserved for later use'" C-m
 
 # Select first pane
 tmux select-pane -t $SESSION_NAME:0.0
