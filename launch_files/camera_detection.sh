56ab5386d7b470ba163873a28620cad8f1836284
nidhi-aifi <nidhi.r@aifitechnologies.com>
2025-11-21 12:05:25 +0400

    model start position changed

--- launch_files/camera_detection.sh
+++ launch_files/camera_detection.sh
@@ -1,4 +1,3 @@
-  GNU nano 6.2                                                                                camera_detection.sh                                                                                          
 #!/bin/bash
 
 # PX4 + ROS2 Humble + uXRCE-DDS tmux launcher
@@ -10,6 +9,7 @@ ROS_DOMAIN_ID=${ROS_DOMAIN_ID:-3}
 PX4_UXRCE_DDS_PORT=${PX4_UXRCE_DDS_PORT:-8888}
 PX4_UXRCE_DDS_NS=${PX4_UXRCE_DDS_NS:-cam_drone}
 PX4_UAV_MODEL=${PX4_UAV_MODEL:-gz_x500_depth}
+PX4_GZ_MODEL_POSE="268.08,-128.22,3.86,0.00,0,-0.7"
 
 # Kill any existing session
 tmux kill-session -t $SESSION_NAME 2>/dev/null
@@ -18,7 +18,7 @@ tmux kill-session -t $SESSION_NAME 2>/dev/null
 tmux new-session -d -s $SESSION_NAME
 
 # Pane 0: PX4 SITL + Gazebo headless
-tmux send-keys -t $SESSION_NAME "cd /app/PX4-Autopilot/; ROS_DOMAIN_ID=$ROS_DOMAIN_ID PX4_UXRCE_DDS_PORT=$PX4_UXRCE_DDS_PORT PX4_UXRCE_DDS_NS=$PX4_UXRCE_DDS_NS make px4_sitl $PX4_UAV_MODEL HEADLESS=1" C>
+tmux send-keys -t $SESSION_NAME "cd /app/PX4-Autopilot/; ROS_DOMAIN_ID=$ROS_DOMAIN_ID PX4_UXRCE_DDS_PORT=$PX4_UXRCE_DDS_PORT PX4_UXRCE_DDS_NS=$PX4_UXRCE_DDS_NS PX4_GZ_MODEL_POSE=\"$PX4_GZ_MODEL_POSE\"  make px4_sitl $PX4_UAV_MODEL HEADLESS=1" C-m
 
 # Pane 1: Micro XRCE-DDS Agent
 tmux split-window -h -t $SESSION_NAME
@@ -35,15 +35,17 @@ tmux split-window -v -t $SESSION_NAME:0.0
 tmux send-keys -t $SESSION_NAME:0.2 'bash -c "source /opt/ros/humble/setup.bash && source /app/ros2_ws/install/setup.bash && sleep 6 && ros2 run ros_gz_image image_bridge /camera"' C-m
 
 # Pane 3: Gazebo GUI (optional, can attach to same simulation)
-tmux split-window -v -t $SESSION_NAME:0.0
-tmux send-keys -t $SESSION_NAME:0.3 "gz sim /app/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf" C-m
+sim_pane=$(tmux split-window -v -t $SESSION_NAME:0.0 -P -F "#{pane_id}")
+tmux send-keys -t $sim_pane "sleep 5; gz sim /app/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf" C-m
 
 # Create a new tmux window for ROS2 tools
 tmux new-window -t $SESSION_NAME -n "ros2_tools"
 tmux send-keys -t $SESSION_NAME:1 "bash -c 'source /opt/ros/humble/setup.bash && source /app/ros2_ws/install/setup.bash && ros2 topic list'" C-m
 
 # Select first pane in first window
+tmux select-window -t $SESSION_NAME:0
 tmux select-pane -t $SESSION_NAME:0.0
+
 
 # Attach to tmux session
 tmux attach-session -t $SESSION_NAME
